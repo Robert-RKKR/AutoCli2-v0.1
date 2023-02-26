7275a710c99275392b61ac142ad6f926910b2e4e
robert-rkkr <robert.kucharski.rkkr@gmail.com>
2023-02-26 14:03:43 +0100

    Model update

--- backend/autocli2/settings.py
+++ backend/autocli2/settings.py
@@ -132,22 +132,22 @@ REST_FRAMEWORK = {
 }
 
 # Database:
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-#         "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-#         "USER": os.environ.get("SQL_USER", "user"),
-#         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-#         "HOST": os.environ.get("SQL_HOST", "localhost"),
-#         "PORT": os.environ.get("SQL_PORT", "5432"),
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation:
 AUTH_PASSWORD_VALIDATORS = [
--- backend/inventory/models/__init__.py
+++ backend/inventory/models/__init__.py
@@ -1,6 +1,8 @@
 # Application iImport:
 from .credentials import Credential
+from .api_setting import ApiSetting
 from .platform import Platform
+from .software import Software
 from .region import Region
 from .site import Site
 from .host import Host
