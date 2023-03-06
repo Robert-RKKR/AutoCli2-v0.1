972666ee6e634942828d4a7ffd664acfa369e71d
Robert-RKKR <64611240+Robert-RKKR@users.noreply.github.com>
2023-03-06 11:55:46 +0100

    T

--- backend/notification/admin.py
+++ backend/notification/admin.py
@@ -15,6 +15,9 @@ class NotificationAdmin(admin.ModelAdmin):
         'object_representation', 'severity', 'notification_type',
         'task_id', 'application', 'message',
     )
+    list_display_links = (
+        'pk',
+    )
     list_filter = (
         'app_name', 'administrator', 'model_name', 'action_type',
         'notification_type', 'severity', 'application',
@@ -47,6 +50,7 @@ class NotificationAdmin(admin.ModelAdmin):
         'notification_type', 'task_id', 'application', 'message', 'app_name',
         'model_name', 'object_representation', 'object_id', 'administrator', 
     )
+    empty_value_display = '--None--'
 
 
 @admin.register(ChangeLog)
@@ -55,6 +59,9 @@ class ChangLogAdmin(admin.ModelAdmin):
     list_display = (
         'pk', 'administrator', 'timestamp', 'action_type',
         'object_representation', 'after',
+    )
+    list_display_links = (
+        'pk',
     )
     list_filter = (
         'app_name', 'administrator', 'model_name', 'action_type',
@@ -78,3 +85,4 @@ class ChangLogAdmin(admin.ModelAdmin):
         'administrator', 'timestamp', 'action_type', 'administrator', 'app_name',
         'object_representation', 'after', 'object_id', 'model_name', 
     )
+    empty_value_display = '--None--'
