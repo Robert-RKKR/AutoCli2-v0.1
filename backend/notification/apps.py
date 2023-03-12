58493ac6ffefe3cb89575041ac7a38d69be4aa43
robert-rkkr <robert.kucharski.rkkr@gmail.com>
2023-03-12 20:56:15 +0100

    Notification

--- backend/notification/apps.py
+++ backend/notification/apps.py
@@ -7,5 +7,5 @@ class NotificationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notification'
 
-    def ready(self):
-        import notification.signals
+    # def ready(self):
+    #     import notification.signals
