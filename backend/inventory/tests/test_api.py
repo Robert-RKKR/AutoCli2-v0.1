0de9f53c99b1a83ded351a8a2ca652390c847498
robert-rkkr <robert.kucharski.rkkr@gmail.com>
2023-03-19 23:16:02 +0100

    API - Inventory

--- backend/inventory/tests/test_api.py
+++ backend/inventory/tests/test_api.py
@@ -55,115 +55,126 @@ class InventoryApiTest(BaseApiTest):
 
 class TestInventoryApi(InventoryApiTest):
 
-    def test_api_create(self):
-        # Collect responses:
-        responses = []
+    # def test_api_create(self):
+    #     # Collect responses:
+    #     responses = []
 
-        # Create test API credential object:
-        response = self.client.post(
-            self.credential, self.credential_data, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Create test API platform object:
-        response = self.client.post(
-            self.platform, self.platform_data, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Create test API region object:
-        response = self.client.post(
-            self.region, self.region_data, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Create test API site object:
-        response = self.client.post(
-            self.site, self.site_data, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Create test API host object:
-        response = self.client.post(
-            self.host, self.host_data, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
+    #     # Create test API credential object:
+    #     response = self.client.post(
+    #         self.credential, self.credential_data, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Create test API platform object:
+    #     response = self.client.post(
+    #         self.platform, self.platform_data, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Create test API region object:
+    #     response = self.client.post(
+    #         self.region, self.region_data, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Create test API site object:
+    #     response = self.client.post(
+    #         self.site, self.site_data, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Create test API host object:
+    #     response = self.client.post(
+    #         self.host, self.host_data, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
  
-        # Check responses status
-        self.assertEqual(True, self.check_status_code_list(201, responses))
+    #     # Check responses status
+    #     self.assertEqual(True, self.check_status_code_list(201, responses))
 
-    def test_api_list(self):
-        # Collect responses:
-        responses = []
+    # def test_api_list(self):
+    #     # Collect responses:
+    #     responses = []
 
-        # Collect test api credential object:
-        response = self.client.get(
-            self.credential, params={'id': '1'}, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Collect test api platform object:
-        response = self.client.get(
-            self.platform, params={'id': '1'}, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Collect test api region object:
-        response = self.client.get(
-            self.region, params={'id': '1'}, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Collect test api site object:
-        response = self.client.get(
-            self.site, params={'id': '1'}, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Collect test api host object:
-        response = self.client.get(
-            self.host, params={'id': '1'}, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
+    #     # Collect test api credential object:
+    #     response = self.client.get(
+    #         self.credential, params={'id': '1'}, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Collect test api platform object:
+    #     response = self.client.get(
+    #         self.platform, params={'id': '1'}, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Collect test api region object:
+    #     response = self.client.get(
+    #         self.region, params={'id': '1'}, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Collect test api site object:
+    #     response = self.client.get(
+    #         self.site, params={'id': '1'}, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Collect test api host object:
+    #     response = self.client.get(
+    #         self.host, params={'id': '1'}, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
         
-        # Check responses status
-        self.assertEqual(True, self.check_status_code_list(200, responses))
+    #     # Check responses status
+    #     self.assertEqual(True, self.check_status_code_list(200, responses))
 
-    def test_api_retrieve(self):
-        # Collect responses:
-        responses = []
+    # def test_api_retrieve(self):
+    #     # Collect responses:
+    #     responses = []
 
-        # Collect test api credential object:
-        response = self.client.get(
-            self.credential, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Collect test api platform object:
-        response = self.client.get(
-            self.platform, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Collect test api region object:
-        response = self.client.get(
-            self.region, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Collect test api site object:
-        response = self.client.get(
-            self.site, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
-        # Collect test api host object:
-        response = self.client.get(
-            self.host, format='json')
-        # Add response status code to responses list:
-        responses.append(response.status_code)
+    #     # Collect test api credential object:
+    #     response = self.client.get(
+    #         self.credential, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Collect test api platform object:
+    #     response = self.client.get(
+    #         self.platform, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Collect test api region object:
+    #     response = self.client.get(
+    #         self.region, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Collect test api site object:
+    #     response = self.client.get(
+    #         self.site, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
+    #     # Collect test api host object:
+    #     response = self.client.get(
+    #         self.host, format='json')
+    #     # Add response status code to responses list:
+    #     responses.append(response.status_code)
         
-        # Check responses status
-        self.assertEqual(True, self.check_status_code_list(200, responses))
+    #     # Check responses status
+    #     self.assertEqual(True, self.check_status_code_list(200, responses))
 
     def test_api_update(self):
         # Collect responses:
         responses = []
 
+        # Create
+        create = self.client.post(
+            self.credential, self.credential_data, format='json')
+        
+        get_all = self.client.get(
+            self.credential, format='json')
+
         # prepare test api data for PUt method:
         self.credential_data['name'] = 'A new test credential'
         # Collect test api credential object:
-        response = self.client.put(
-            self.credential + '1', self.credential_data, format='json')
-        print('======> ', response)
+        edit = self.client.put(self.credential + '1/', self.credential_data, format='json')
+        
+        get_one = self.client.get(
+            self.credential, params={'id': '1'}, format='json')
+
+        
+        print('======> ', create.status_code, get_all.content, edit.status_code, get_one.content)
         # Add response status code to responses list:
         responses.append(301)
         
