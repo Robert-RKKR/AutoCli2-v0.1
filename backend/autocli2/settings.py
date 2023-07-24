a1c70ce4edadf087e40cb77ad5c85d2d9823c5c4
robertrkkr <robert.kucharski.rkkr@gmail.com>
2023-07-24 17:09:26 +0200

    DB

--- backend/autocli2/settings.py
+++ backend/autocli2/settings.py
@@ -156,13 +156,13 @@ REST_FRAMEWORK = {
 
 # Database:
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),
-        "PORT": os.environ.get("SQL_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'autocli',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': 5005,
     }
 }
 
